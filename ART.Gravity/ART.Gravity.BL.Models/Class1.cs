8067474ff5baae4f82229adcb1cfabdc1d86c26c
Adam Tamminga <500158560@fvtc.edu>
2021-01-23 14:12:40 -0600

    Added BL.Models Gravity class

--- ART.Gravity/ART.Gravity.BL.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ART.Gravity.BL.Models
-{
-    public class Class1
-    {
-    }
-}
---
+++ ART.Gravity/ART.Gravity.BL.Models/Gravity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ART.Gravity.BL.Models
+{
+    public class Gravity
+    {
+        public Guid Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public float Mass1 { get; set; }
+        public float Mass2 { get; set; }
+        public float Distance { get; set; }
+        public float Force { get; set; }
+    }
+}
