db092e453563e8ee1c7a1aca58f3cad08abecf2c
Adam Tamminga <500158560@fvtc.edu>
2021-01-23 14:16:49 -0600

    Added the equation to the model

--- ART.Gravity/ART.Gravity.BL.Models/Gravity.cs
+++ ART.Gravity/ART.Gravity.BL.Models/Gravity.cs
@@ -4,6 +4,8 @@ namespace ART.Gravity.BL.Models
 {
     public class Gravity
     {
+
+        // Force = G * (M1 * M2/ r^2)
         public Guid Id { get; set; }
         public DateTime ChangeDate { get; set; }
         public float Mass1 { get; set; }
