c87c7a31dfd370f07391ea58193bd24f42b90f9e
Adam Tamminga <500158560@fvtc.edu>
2021-01-26 21:03:23 -0600

    Added PL

---
+++ ART.Gravity/ART.Gravity.PL/TblGravity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ART.Gravity.PL
+{
+    public partial class tblGravity
+    {
+        public Guid Id { get; set; }
+        public DateTime ChangeDate { get; set; }
+        public double MassOne { get; set; }
+        public double MassTwo { get; set; }
+        public double Distance { get; set; }
+        public double Force { get; set; }
+    }
+}
